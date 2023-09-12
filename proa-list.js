4254e7f9062b0362062606c99481e4ff47fd6c24
Lívia V. Gomes <114452531+ViviSGG@users.noreply.github.com>
2023-09-12 00:22:21 -0300

    Initial commit

---
+++ proa-list.js
@@ -0,0 +1,70 @@
+/* 1. Crie um programa onde o usuário possa cadastrar estudantes sem limites, e, em seguida, Se o usuário digitar "PARE" o programa deve exibir a quantidade de estudantes cadastrados e a lista com cada um deles.  */
+
+let cadastro = [];
+let loop = 0;
+
+function questao1() {
+    do {
+        cadastro.push({
+            nome: prompt("nome: "),
+            idade: parseInt(prompt("idade: ")),
+            escolaridade: prompt("escolaridade: ")
+        });
+        loop = parseInt(prompt("[1] CONTINUAR [2] PARAR"));
+
+    } while (loop != 2);
+    
+    console.log(cadastro);
+}
+
+/* 2. Crie uma array de planetas que inclua "Terra", "Marte", "Plutão", "Vênus", "Júpiter", "Saturno"  e, em seguida, peça ao usuário para digitar o nome de um planeta. 
+
+Verifique se o planeta que o usuário informou está na array e informe ao usuário.
+ */
+
+let planetas = ["Terra","Marte","Plutão","Vênus","Júpiter","Saturno"];
+
+function questao2() {
+    resp = prompt("Digite o nome de um planeta:");
+
+    if (planetas.includes(resp)) {
+        document.write(`${resp} faz parte da lista!`);
+    } else {
+        document.write(`${resp} não faz parte da lista!`);
+    }
+}
+
+/* 3. Vamos criar uma lista de compras. 
+
+Crie uma array de frutas, exiba-a ao usuário e, em seguida, peça ao usuário para digitar o nome de uma das-fruta.
+
+Caso a fruta esteja no array, remova-a e exiba a mensagem "Fruta foi retirada da lista". 
+
+Peça novamente para o usuário digitar o nome de uma fruta para ser removida. 
+
+Sempre que o usuário procurar por uma que não está no array exiba a mensagem "Fruta indisponível no nosso mercado". 
+
+Quando o array não possuir mais itens dentro de si, escreva "Lista de compras finalizada".
+ */
+
+let frutas = ["manga", "maçã", "pera", "uva", "morango"];
+// let tamanho = frutas.length;
+// console.log(tamanho);
+
+function questao3() {
+    // while (frutas.length < 1) { /* length - quantidade de itens dentro do array */
+        let novaFruta = prompt(`Frutas na lista: ${frutas.join(", ")}. Qual deseja?`);
+
+        if (frutas.includes(novaFruta)) {
+            let continuar = prompt(`A fruta ${novaFruta} está disponível! Deseja comprar? (S/N)`);
+            if (continuar == "S" || continuar == "s") {
+                let remover = frutas.indexOf(novaFruta);
+                console.log(continuar);
+                frutas.splice(remover, 1);
+            } 
+            console.log(frutas);
+        } else {
+            alert(`A fruta ${novaFruta} não está disponível!`);
+        }
+    // } 
+}
